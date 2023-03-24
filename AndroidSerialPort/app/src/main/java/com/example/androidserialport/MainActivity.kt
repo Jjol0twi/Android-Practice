a788205dc6ca06d196bf6a56c4a3a0049c5b01cc
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-03-24 14:23:57 +0900

    temporary setting serialport

--- AndroidSerialPort/app/src/main/java/com/example/androidserialport/MainActivity.kt
+++ AndroidSerialPort/app/src/main/java/com/example/androidserialport/MainActivity.kt
@@ -1,32 +1,48 @@
 package com.example.androidserialport
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
-import android.widget.TextView
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidserialport.android_serialport_api.SerialPort
 import com.example.androidserialport.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mSerialport: SerialPort
+    private val serialPORT = File("/dev/ttysWK0")
+    private val serialBAUDRATE = 115200
 
-    companion object{
-        fun load(){
+    companion object {
+        fun load() {
             System.loadLibrary("serial_port")
         }
     }
 
     init {
-        load()
+//        load()
+        try {
+            System.loadLibrary("serial_port")
+        } catch (e: Exception) {
+            Log.d("error", e.toString())
+        }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
-        val text_main=binding.textMain
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        try {
+            mSerialport = SerialPort(serialPORT, serialBAUDRATE, 0)
+        } catch (e: Exception) {
+            Log.d("error", e.toString())
+        }
+        val textMain = binding.textMain
         setContentView(binding.root)
     }
 
-
+    override fun onDestroy() {
+        super.onDestroy()
+        mSerialport.close()
+    }
 
 }
--- AndroidSerialPort/app/src/main/java/com/example/androidserialport/util/SerialPortUtil.kt
+++ AndroidSerialPort/app/src/main/java/com/example/androidserialport/util/SerialPortUtil.kt
@@ -1,5 +1,27 @@
 package com.example.androidserialport.util
 
+import android.util.Log
+import com.example.androidserialport.android_serialport_api.SerialPort
+import java.io.File
+
 class SerialPortUtil {
-    
+    private final val serialPORT = File("dev/ttysWK0")
+    private final val serialBAUDRATE = 115200
+
+    init {
+        try {
+            SerialPort(serialPORT, serialBAUDRATE, 0)
+        } catch (e: Exception) {
+            Log.d("error", e.toString())
+        }
+    }
+
+
+    private fun setCommand(c: String) {
+
+    }
+
+    private fun readResponse() {
+
+    }
 }
