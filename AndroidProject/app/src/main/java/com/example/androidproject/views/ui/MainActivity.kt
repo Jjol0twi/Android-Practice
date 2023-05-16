a9914f0e02835b375d64c52b5a749cddf370e780
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-05-16 15:27:50 +0900

    add hideui

--- AndroidProject/app/src/main/java/com/example/androidproject/views/ui/MainActivity.kt
+++ AndroidProject/app/src/main/java/com/example/androidproject/views/ui/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.androidproject.views.ui
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import com.example.androidproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun hideSystemui() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.hide(WindowInsets.Type.systemBars() or WindowInsets.Type.navigationBars())
+            window.insetsController?.systemBarsBehavior =
+                WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        } else {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE    // 가장자리 스와이프 시 발동
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN    // full screen mode
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION  // hide bottom navigation bar
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        }
     }
 }
