1dd040a1a93f7114d25e8cba7fcd883a956dbad5
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-05-16 16:41:42 +0900

    modified hideui&add explanation

--- AndroidProject/app/src/main/java/com/example/androidproject/views/ui/MainActivity.kt
+++ AndroidProject/app/src/main/java/com/example/androidproject/views/ui/MainActivity.kt
@@ -17,9 +17,11 @@ class MainActivity : AppCompatActivity() {
     private fun hideSystemui() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.setDecorFitsSystemWindows(false)
-            window.insetsController?.hide(WindowInsets.Type.systemBars() or WindowInsets.Type.navigationBars())
-            window.insetsController?.systemBarsBehavior =
-                WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            window.insetsController?.let {
+                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())   // or로 사용하는 이유는 systembars(0x00000007), navigationbars(0x00000200)로 결합한 값으로 설정한다.
+                it.systemBarsBehavior = // systembar의 동작을 제어
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE    // Todo"이 속성에 대한 설명"
+            }
         } else {
             window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE    // 가장자리 스와이프 시 발동
                     or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
