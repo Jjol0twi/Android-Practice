2d9a0f91e9baaba460728653de58138d29914170
Hong Hyun Min <jjol_twi@Hongui-MacBookPro.local>
2024-02-27 19:00:05 +0900

    apply .gitignore

--- AndroidProject/serialport/src/main/obj/local/arm64-v8a/objs/serial_port/SerialPort.o.d
+++
@@ -1,5 +0,0 @@
-/Users/honghyunmin/Private/android-project/AndroidSerialPort/app/src/main/obj/local/arm64-v8a/objs/serial_port/SerialPort.o: \
-  /Users/honghyunmin/Private/android-project/AndroidSerialPort/app/src/main/jni/SerialPort.c \
-  /Users/honghyunmin/Private/android-project/AndroidSerialPort/app/src/main/jni/SerialPort.h
-
-/Users/honghyunmin/Private/android-project/AndroidSerialPort/app/src/main/jni/SerialPort.h:
--- MyApplicationWithXML/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplicationWithXML/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding //xml이름+Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.mainTxt.text = "binding.아까 layout에 설정한 id"
+        with(binding){
+            mainTxt.text = ""
+            subTxt.text=""
+        }
+        setContentView(binding.root)
     }
 }
