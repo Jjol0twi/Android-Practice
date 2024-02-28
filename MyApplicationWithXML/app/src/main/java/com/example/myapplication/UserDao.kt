767359cb0d49531588c1174c8788370dc963ef91
Hong Hyun Min <jjol_twi@Hongui-MacBookPro.local>
2024-02-28 16:18:52 +0900

    add user data dao

---
+++ MyApplicationWithXML/app/src/main/java/com/example/myapplication/UserDao.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import androidx.room.Dao
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getAll(): List<User>
+
+    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
+    fun loadAllByIds(userIds: IntArray): List<User>
+
+
+}
