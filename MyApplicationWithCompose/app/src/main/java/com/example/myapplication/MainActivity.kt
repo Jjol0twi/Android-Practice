6ddfce3be07195d6a39049144d2c229646f2d2d6
Hong Hyun Min <jjol_twi@Hongui-MacBookPro.local>
2024-02-28 16:15:55 +0900

    disable mainactivity preview

--- MyApplicationWithCompose/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplicationWithCompose/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -29,18 +29,18 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationWithComposeTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MyApplicationWithComposeTheme{
+//        Greeting("Android")
+//    }
+//}
