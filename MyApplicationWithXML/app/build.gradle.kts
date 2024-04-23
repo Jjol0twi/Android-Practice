7160b684c1230812df77ac9232b11ceaa287fa1c
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2024-04-23 16:45:32 +0900

    mod android package name

--- MyApplicationWithXML/app/build.gradle.kts
+++ MyApplicationWithXML/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.ihateham.androidsampleproject"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.ihateham.androidsampleproject"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
