d621451c7caca22ce2881aa88665d537efb4d60e
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2024-04-29 19:50:12 +0900

    save api key to utils

---
+++ MyApplicationWithXML/app/src/main/java/com/ihateham/androidsampleproject/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.ihateham.androidsampleproject.utils
+
+import com.ihateham.androidsampleproject.BuildConfig
+
+object Constants {
+    const val NAVER_CLIENT_ID = BuildConfig.NAVER_CLIENT_ID
+    const val NAVER_CLIENT_SECRET = BuildConfig.NAVER_CLIENT_SECRET
+    const val NAVER_CLIENT_NAME = BuildConfig.NAVER_CLIENT_NAME
+    const val GOOGLE_WEB_CLIENT = BuildConfig.GOOGLE_WEB_CLIENT
+}
