e0101b782204c3a133d940c02f0970439a986ad2
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2024-04-29 19:51:57 +0900

    add google social login

--- MyApplicationWithXML/app/src/main/java/com/ihateham/androidsampleproject/presentation/signin/SignInActivity.kt
+++ MyApplicationWithXML/app/src/main/java/com/ihateham/androidsampleproject/presentation/signin/SignInActivity.kt
@@ -1,16 +1,56 @@
 package com.ihateham.androidsampleproject.presentation.signin
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 import com.ihateham.androidsampleproject.databinding.ActivitySignInBinding
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.OAuthLoginCallback
+
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract(),
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
+    private fun googleLogin() {
+        val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            val user = FirebaseAuth.getInstance().currentUser
+            Toast.makeText(this, "${user?.uid}",Toast.LENGTH_SHORT).show()
+            Log.i("success", "get google login ${user?.uid}")
+        } else {
+            Log.i("fail", "login signup${response}")
+        }
+    }
+
     private val oauthLoginCallback = object : OAuthLoginCallback {
         override fun onSuccess() {
             Log.i("success", "get access token(${NaverIdLoginSDK.getAccessToken()})")
