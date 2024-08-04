c2d3ae088c1cc099f177625db4f64f841f0a8c0e
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2024-08-05 00:54:22 +0900

    delcare sqlite on application

--- MyApplicationWithCompose/app/src/main/java/com/Ihateham/androidpractice/MyApplication.kt
+++ MyApplicationWithCompose/app/src/main/java/com/Ihateham/androidpractice/MyApplication.kt
@@ -1,8 +1,10 @@
 package com.Ihateham.androidpractice
 
 import android.app.Application
+import com.Ihateham.androidpractice.db.local.SQLiteHelper
 
 class MyApplication : Application() {
+    val sqlite: SQLiteHelper by lazy { SQLiteHelper.getInstance(this) }
     override fun onCreate() {
         super.onCreate()
     }
